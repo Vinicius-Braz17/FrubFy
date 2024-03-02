d29999a4ada0ff2976747a49efbf3208f10b9cd4
Vinicius-Braz17 <godinhobraz@gmail.com>
2024-03-01 21:24:09 -0300

    Conexão com servidor de banco de dados, finalizacao de codigos para funcionar, apenas ajustes finais e transição de todos os codigos

--- src/pages/ElementosKanBans/CriarCard.js
+++ src/pages/ElementosKanBans/CriarCard.js
@@ -38,7 +38,6 @@ function CriarCard({ fecharPainel, BD, Ids }) {
     },
   ];
 
-  
   const [cargos, setCargos] = useState([]);
   const [filiais, setFiliais] = useState([]);
   
@@ -46,12 +45,17 @@ function CriarCard({ fecharPainel, BD, Ids }) {
 
   const sp = createClient("https://vlyrqlntoaqrrafufybv.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZseXJxbG50b2FxcnJhZnVmeWJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkzMzE0NDYsImV4cCI6MjAyNDkwNzQ0Nn0.CS21CLVfZKFD66Elev9DfcsXQD36IO9R6us7ieXbOVA");
 
+  async function insertCol() {
+    const {error} = await sp.from("candidatos").delete().eq('id_candidato', 7);
+  }
   async function getCargos() {
     const { data } = await sp.from("cargos").select();
+    
     setCargos(data);
   }
   useEffect(() => {
     getCargos();
+    insertCol()
   });
 
   // useEffect(() => {
